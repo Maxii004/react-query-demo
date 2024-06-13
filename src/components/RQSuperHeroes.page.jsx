6341d6b3b974dd253c56c19393987773febbbcea
Mineth Perera <mineth.perera@gmail.com>
2024-06-13 16:38:14 +0530

    feat: fetching data on click

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -5,9 +5,12 @@ export const RQSuperHeroesPage = () => {
   const fetchSuperHeroes = () => {
     return axios.get("http://localhost:4000/superheroes");
   };
-  const { data, isLoading, isError, error } = useQuery(
+  const { data, isLoading, isError, error, refetch } = useQuery(
     "super-heroes",
-    fetchSuperHeroes
+    fetchSuperHeroes,
+    {
+      enabled: false, // Disable fetching data on mount
+    }
   );
 
   if (isLoading) {
@@ -21,6 +24,7 @@ export const RQSuperHeroesPage = () => {
   return (
     <div>
       <h1>RQ Super Heroes Page</h1>
+      <button onClick={refetch}>Fetch heroes</button>
       <ul>
         {data?.data.map((hero) => (
           <li key={hero.id}>{hero.name}</li>
